0e61eb340e6263024ef862d0483f4b775fb30860
CrablinD1 <64089854+CrablinD1@users.noreply.github.com>
2021-02-09 17:39:53 +0300

    Blog: Data & Models

--- my_site/settings.py
+++ my_site/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'book_outlet',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
